a41c8dfcc4fa655fd3147f8efd8fa0808908bcd9
Adriano Teixeira João <75324315+adriano2teixeira@users.noreply.github.com>
2021-08-25 09:48:57 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const server = http.createServer(api)
 const PORT = process.env.PORT | 3001
 
 server.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port);
 });
