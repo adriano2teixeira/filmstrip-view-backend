0ac14de8cd6d8946f9755a451ae14991f57496c7
Adriano Teixeira João <75324315+adriano2teixeira@users.noreply.github.com>
2021-08-25 09:43:23 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,4 +4,4 @@ const api = require("./app")
 const server = http.createServer(api)
 const PORT = process.env.PORT | 3001
 
-server.listen(PORT, () => console.log(`Server Listeing to http://localhost:3001`))
+server.listen(PORT, () => console.log(`Server Listeing to http://localhost:${PORT}`))
